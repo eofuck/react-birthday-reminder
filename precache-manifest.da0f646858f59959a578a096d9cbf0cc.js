631a23ae4a4778dc047edc37e84f07f450e56bab
eofuck <gkublashvili002@gmail.com>
2021-08-08 17:25:56 +0400

    Updates

---
+++ precache-manifest.da0f646858f59959a578a096d9cbf0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c024ad3c147885f01d166ad0f0c0158",
+    "url": "/react-birthday-reminder/index.html"
+  },
+  {
+    "revision": "92154354670e64537135",
+    "url": "/react-birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92154354670e64537135",
+    "url": "/react-birthday-reminder/static/js/main.be8ead71.chunk.js"
+  },
+  {
+    "revision": "ca18d9339a309e58c265",
+    "url": "/react-birthday-reminder/static/js/runtime-main.4fe2f5be.js"
+  }
+]);
